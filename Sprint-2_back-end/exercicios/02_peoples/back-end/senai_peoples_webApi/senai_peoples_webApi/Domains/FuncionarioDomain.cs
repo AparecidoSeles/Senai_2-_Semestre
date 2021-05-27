8e520e5a71c31d40aaecb70fc1c994c847e4cbc1
Aparecido Seles <seles411@gmail.com>
2021-05-27 14:45:49 -0300

    exercicio relogio concluído

---
+++ Sprint-2_back-end/exercicios/02_peoples/back-end/senai_peoples_webApi/senai_peoples_webApi/Domains/FuncionarioDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Peoples_WebAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade Funcionarios
+    /// </summary>
+    public class FuncionarioDomain
+    {
+        //Campo 'IdFuncionario' da tabela do banco de dados
+        public int IdFuncionario { get; set; }
+
+        //Campo 'Nome' da tabela do banco de dados
+        public string Nome { get; set; }
+
+        //Campo 'Sobrenome' da tabela do banco de dados
+        public string Sobrenome { get; set; }
+    }
+}
