1996003f183b20e26735100f1b3cc0b44e0ce03b
Aparecido Seles <seles411@gmail.com>
2021-06-04 15:50:31 -0300

    sp_medical_group-ui Criada

---
+++ Sprint-4_Front-end/sp_medical_group-ui/src/index.js
@@ -0,0 +1,24 @@
+//Referencia de Bibliotecas
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Arquivos css
+import './index.css';
+
+//chamada de Paginas
+import App from './pages/home/App';
+
+//Arquivos de performance
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ Sprint-4_Front-end/sp_medical_group-ui/src/pages/TiposEventos/TiposEventos.js
@@ -0,0 +1,20 @@
+import {Component } from 'react'; 
+
+class TiposEventos extends Component{
+    constructor (props){
+       super(props);
+       this.state = {
+            //NomeEstado : valor Inicial
+            listaTiposEventos : [],
+            titulo : ''
+        }
+    }
+    
+    componentDidMount(){
+        //código
+
+    }
+    render(){
+        return
+    }
+}
